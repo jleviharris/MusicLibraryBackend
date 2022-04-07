61be40551e3beb0e15dfdf214d0f6836bebccbba
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-04-07 16:55:03 -0400

    Added middleware and started server

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-
 // Imports
 const express = require('express');
 const app = express();
+
+
+// Middleware
+app.use(express.json());
+
+
+
+// Starting Server
+const PORT = process.env.PORT || 5005;
+
+app.listen(PORT, () => {
+    console.log(`Erver running on PORT: ${PORT}`);
+});
+
+
+
+
 
 
 
